8041c7d73b73300573b282b2ff7936bce22773d4
mdasadul <mdasadul@ualberta.ca>
2016-04-12 14:39:47 -0500

    adding predicting next word using theno

--- mnist.py
+++ mnist.py
@@ -1,8 +1,22 @@
 import input_data
 import tensorflow as tf
 
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev=0.1)
+    return tf.Variable(initial)
 
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape = shape)
+    return tf.Variable(initial)
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,strides =[1,1,1,1],padding ='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize = [1,2,2,1],strides=[1,2,2,1],padding ='SAME')
+
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32,[None,784])
 
 W=tf.Variable(tf.zeros([784,10]))
@@ -16,17 +30,17 @@ y_ = tf.placeholder(tf.float32,[None,10])
 
 cross_enropy = -tf.reduce_sum(y_*tf.log(y))
 
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_enropy)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_enropy)
 
 init = tf.initialize_all_variables()
 
-sess = tf.Session()
+
 sess.run(init)
-for i in range(10000):
+for i in range(1000):
     batch_xs,batch_ys = mnist.train.next_batch(50)
     sess.run(train_step,feed_dict={x:batch_xs,y_:batch_ys})
     
 correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
-print(sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
+print(accuracy.eval({x:mnist.test.images,y_:mnist.test.labels}))
---
+++ mnist_interactive.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+import input_data
+
+data_sets = input_data.read_data_sets(train_dir)
